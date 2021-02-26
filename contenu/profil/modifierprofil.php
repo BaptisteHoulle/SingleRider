3d0d3772a550f85518c574663ada5083d28b0c67
Coline <coline.canon@gmail.com>
2021-02-26 16:06:27 +0100

    page modifier le profil

--- contenu/profil/modifierprofil.php
+++ contenu/profil/modifierprofil.php
@@ -2,7 +2,6 @@
 
 ?>
 
-
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
 
@@ -10,7 +9,7 @@
     <meta charset="utf-8">
     <title>Profil</title>
     <link rel="stylesheet" href="../../css/main.css">
-    <script src="https://kit.fontawesome.com/531f7c49c9.js"></script> 
+    <script src="https://kit.fontawesome.com/531f7c49c9.js"></script>     
     <link rel="stylesheet" href="file:///E:/fontawesome/css/all.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="file:///E:/jquery.js"></script>
@@ -87,44 +86,140 @@
                 <label for="datenaissance">Date de naissance</label><input type="date" name="datenaissance" id="datenaissance">
             </div>
             <div id="sexe_modif">
-                <label id="titre_sexe">Sexe</label>
-                <input type="radio" id="choix1" name="genre" value="homme">
-                <label for="choix1">Homme</label>
-                <input type="radio" id="choix2" name="genre" value="femme">
-                <label for="choix2">Femme</label>
+                <h2 id="titre_sexe">Sexe</h2>
+                <div class="radiobutton">
+                    <input type="radio" id="choix1_sexe" name="genre" value="homme">
+                    <label for="choix1">Homme</label>
+                </div>
+                <div class="radiobutton">
+                    <input type="radio" id="choix2_sexe" name="genre" value="femme">
+                    <label for="choix2">Femme</label> 
+                </div>
             </div>
             <div id="localisation_modif">
-                <label id="titre_localisation">Localisation</label>
-                <input type="radio" id="choix1" name="localiser" value="oui">
-                <label for="choix1">Oui</label>
-                <input type="radio" id="choix2" name="localiser" value="non">
-                <label for="choix2">Non</label>
+                <h2 class="titre_localisation">Localisation</h2>
+                <div class="radiobutton">
+                    <input type="radio" id="choix_1" name="localiser" value="oui">
+                    <label for="choix1">Oui</label>
+                </div>
+                <div class="radiobutton">
+                    <input type="radio" id="choix_2" name="localiser" value="non">
+                    <label for="choix2">Non</label> 
+                </div>
             </div>
             <div id="titrePreferences">
                 Mes préférences
             </div>
             <div id="preferences">
-                
+                <div id="titre_discussion">
+                    <h2>Discussion :</h2>
+                </div>
+                <div id="icon1_pref">
+                    <i class="fab fa-discourse"></i>
+                </div>
+                <div id="discussion_modif">
+                    <input type="radio" id="choix1" name="discussion" value="discret">
+                    <label for="choix1">Discrète(e)</label>
+                    <input type="radio" id="choix2" name="discussion" value="alaise">
+                    <label for="choix2">Plutôt à l'aise</label>
+                    <input type="radio" id="choix3" name="discussion" value="parlebeaucoup">
+                    <label for="choix2">Parle beaucoup</label>
+                </div>
+                <div id="titre_preference">
+                    <h2>Préférences :</h2>
+                </div>
+                <div id="icon2_pref">
+                    <i class="fas fa-heart"></i>
+                </div>
+                <div id="preferences_modif">
+                    <input type="checkbox" id="genrepref1" name="genrepref1" value="Homme">
+                    <label for="vehicle1"> Homme</label>
+                    <input type="checkbox" id="genrepref2" name="genrepref2" value="Femme">
+                    <label for="vehicle2"> Femme</label>
+                    <input type="checkbox" id="genrepref3" name="genrepref3" value="LesDeux">
+                    <label for="vehicle3"> Les deux</label>
+                </div>
+                <div id="titre_typerecherche">
+                    <h2>Types de recherches :</h2>
+                </div>
+                <div id="icon3_pref">
+                    <i class="fas fa-search"></i>
+                </div>
+                <div id="recherche_modif">
+                    <input type="checkbox" id="type1" name="type1" value="samuser">
+                    <label for="vehicle1">S'amuser</label>
+                    <input type="checkbox" id="type2" name="type2" value="amour">
+                    <label for="vehicle2">Rencontre amoureuse</label>
+                    <input type="checkbox" id="type3" name="type3" value="amis">
+                    <label for="vehicle3">Se faire des amis</label>
+                </div>
+                <div id="titre_typeparc">
+                    <h2>Types de parcs :</h2>
+                </div>
+                <div id="icon4_pref">
+                    <i class="fab fa-fort-awesome-alt"></i>
+                </div>
+                <div id="parc_modif">
+                <input type="checkbox" id="typeparc6" name="typeparc6" value="gsensation">
+                    <label for="vehicle3">Grosse sensation</label> 
+                    <input type="checkbox" id="typeparc5" name="typeparc5" value="sensation">
+                    <label for="vehicle3">Sensation</label> 
+                    <input type="checkbox" id="typeparc2" name="typeparc2" value="proche">
+                    <label for="vehicle2">Proche</label>
+                    <input type="checkbox" id="typeparc3" name="typeparc3" value="aquatique">
+                    <label for="vehicle3">Aquatique</label>
+                    <input type="checkbox" id="typeparc4" name="typeparc4" value="thematise">
+                    <label for="vehicle3">Thématisé</label> 
+                    <input type="checkbox" id="typeparc1" name="typeparc1" value="familiale">
+                    <label for="vehicle1">Familiale</label>
+                </div>
+                <div id="titre_loisir">
+                    <h2>Loisirs :</h2>
+                </div>
+                <div id="icon5_pref">
+                    <i class="fas fa-running"></i>
+                </div>
+                <div id="texte_loisir">
+                    <textarea placeholder="Pêcher, faire du sport, écouter de la musique,voyager.."></textarea>
+                </div>
+                <div id="btn_enregistrer_modif">
+                    <input type="submit" name="enregistrer_modif" value="Enregistrer">
+                </div>
             </div>
-            </form>
-            <!--<div id="titre_mdp">
-                Modifier son mot de passe
+        </div>
+        </form>
+        <hr>
+        <form id="formulaire2" method="post" action="#">
+            <div id="contenu_modif2">
+                <div id="titremodifiermdp">
+                    Modifier son mot de passe
+                </div>
+                <div id="ancienmdp_modif">
+                    Ancien mot de passe<input type="text" name="modifancienmdp" id="modifancienmdp">
+                </div>
+                <div id="newmdp_modif">
+                    Nouveau mot de passe<input type="text" name="modifnewmdp" id="modifnewmdp">
+                </div>
+                <div id="confirmnewnmdp_modif">
+                    Confirmation du nouveau mot de passe<input type="text" name="modifconfirmmdp" id="modifconfirmmdp">
+                </div>
+                <div id="btn_mdp_modif">
+                    <input type="submit" name="mdp_modif" value="Modifier le mot de passe">
+                </div>
             </div>
-            <div id="ancienmdp_modif">
-                Ancien mot de passe :
+        </form>
+        <hr>
+        <div id="deco_supp">
+            <div id="deconnexion_modif">
+                <a id="deco_modif" href="../deconnexion/deconnexion.php">
+                    Déconnexion
+                </a>
             </div>
-            <div id="newmdp_modif">
-                Nouveau mot de passe :
+            <div id="supprimer_modif">
+                <button onclick="confirmDelete()" id="supprcompte_modif">
+                    Supprimer le compte
+                </button> 
             </div>
-            <div id="confirmnewnmdp_modif">
-                Confirmation du nouveau mot de passe :
-            </div>
-            <a id="deco_modif" href="../deconnexion/deconnexion.php">
-                Déconnexion
-            </a>
-            <button onclick="confirmDelete()" id="supprcompte_modif">Supprimer le compte
-            </button>
-
             <script type="text/javascript">
                 function confirmDelete(){
                     var answer = window.confirm("Voulez-vous vraiment supprimer le compte ?");
@@ -136,111 +231,5 @@
                 }
             </script>
         </div>
-
-    <footer>
-        <div id="colonne1">
-            <h2>Réseaux Sociaux</h2>
-            <div id="reseaux1">
-                <h3>Single Rider</h3>
-            </div>
-            <div id="icon1">
-                <i class="fab fa-facebook-square"></i>
-            </div>
-
-            <div id="reseaux2">
-                <h3>@SingleRider</h3>
-            </div>
-            <div id="icon2">
-                <i class="fab fa-twitter"></i>
-            </div>
-            <div id="reseaux3">
-                <h3>@SingleRider</h3>
-            </div>
-            <div id="icon3">
-                <i class="fab fa-instagram"></i>
-            </div>
-            <div id="reseaux4">
-                <h3>Single Rider</h3>
-            </div>
-            <div id="icon4">
-                <i class="fab fa-youtube"></i>
-            </div>
-            <div id="texteReseaux">
-                <h3>Pour nous contacter rapidement ou pour voir les différentes actualités sur les rencontres de Single Rider,
-                    les réseaux sociaux sont un atout car nous répondons rapidement !
-                </h3>
-            </div>
-        </div>
-        <div id="colonne2">
-            <h2>Explorer Single Rider</h2>
-            <div id="page1">
-                <h3>Profil</h3>
-            </div>
-            <div id="page2">
-                <h3>Inscription</h3>
-            </div>
-            <div id="page3">
-                <h3>Connexion</h3>
-            </div>
-            <div id="page4">
-                <h3>Présentation des parcs</h3>
-            </div>
-            <div id="page5">
-                <h3>Actualités</h3>
-            </div>
-            <div id="page6">
-                <h3>Annonces</h3>
-            </div>
-            <div id="page7">
-                <h3>Contact</h3>
-            </div>
-        </div>
-        <div id="colonne3">
-            <h2>Contacter Single Rider</h2>
-            <div id="infos_tel">
-                <h3>Pour toutes informations concernant le site internet où en cas de problème vous pouvez
-                    nous contacter au : 03 24 59 64 70.
-                </h3>
-            </div>
-            <div id="infos_telTab">
-                <h3>03 24 59 64 70</h3>
-            </div>
-            <div id="icon1">
-                <i class="fas fa-phone-alt"></i>                
-            </div>
-            <div id="infos_adresse">
-                <h3>Pour pouvoir nous retrouver :<br>
-                Campus Sup Ardennes, <br>9A Rue Claude Chrétien,<br>08000 Charleville-Mézières</h3>
-            </div>
-            <div id="infos_adresseTab">
-                <h3>Campus Sup Ardennes BP,<br>9A Rue Claude Chrétien,<br>08000 Charleville-Mézières</h3>
-            </div>
-            <div id="icon2">
-                <i class="fas fa-map-marker-alt"></i>
-            </div>
-            <div id="infos_site">
-                <h3>Pour vous rendre chez Single Rider, il suffit de longer la rue perpendiculaire de la 
-                    pataterie en sortie d'autoroute puis de tourner à gauche au bout de la rue !
-                </h3>
-            </div>
-
-            <div id="icon3">
-                <i class="fas fa-route"></i>
-            </div>
-        </div>
-        <div id="mentionslegales">
-            <h3>Mentions légales</h3>
-        </div>
-        <div id="politique">
-            <h3>Politique de confidentialité</h3>
-        </div>
-        <div id="plansite">
-            <h3>Plan du site</h3>
-        </div>
-        <div id="cookies">
-            <h3>Protection sur la vie privée et cookies</h3>
-        </div>
-    </footer>            -->
-
-</body>
+    </body>
 </html>
